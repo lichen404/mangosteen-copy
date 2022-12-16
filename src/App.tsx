70544ea3f56ca6dd750eceb4a3216b668ca926f1
lichen404 <f360family@gmail.com>
2022-12-16 22:42:09 +0800

    使用TSX代替Template

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { defineComponent, ref } from "vue";
+
+export const App = defineComponent({
+  setup() {
+    const refCount = ref(0);
+    return () => (
+      <>
+        <div>{refCount.value}</div>
+        <button
+          onClick={() => {
+            refCount.value++;
+          }}
+        >
+          add
+        </button>
+      </>
+    );
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/mangosteen-copy/dist/',
-  plugins: [vue()]
-})
+  base: "/mangosteen-copy/dist/",
+  plugins: [vue(), vueJsx({ mergeProps: true, transformOn: true })],
+});
