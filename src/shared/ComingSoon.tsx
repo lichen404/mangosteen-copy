05dfd590d403173cc6d6b4db9b1740c5d2e7d188
lichen404 <f360family@gmail.com>
2023-11-28 20:08:50 +0800

    敬请期待页面添加返回按钮

--- src/shared/ComingSoon.tsx
+++ src/shared/ComingSoon.tsx
@@ -1,21 +1,30 @@
-import { defineComponent, PropType } from 'vue'
-import { Center } from './Center'
-import s from './ComingSoon.module.scss'
-import { Icon } from './Icon'
+import { defineComponent, PropType } from "vue";
+import { Center } from "./Center";
+import s from "./ComingSoon.module.scss";
+import { Icon } from "./Icon";
+import { Button } from "./Button";
+import { useRouter } from "vue-router";
 export const ComingSoon = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
+    const router = useRouter();
+    const onClick = () => {
+      router.back();
+    };
     return () => (
       <div>
         <Center class={s.pig_wrapper}>
           <Icon name="pig" class={s.pig} />
         </Center>
         <p class={s.text}>敬请期待</p>
+        <p class={s.link}>
+          <Button onClick={onClick}>返回</Button>
+        </p>
       </div>
-    )
-  }
-})
+    );
+  },
+});
